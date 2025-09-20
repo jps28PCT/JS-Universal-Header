81f5be8bd8602801421b21505323318ad2893e54
jps28PCT <jps28@pct.edu>
2025-09-20 19:08:08 -0400

    Update js_univ.h
    Updated value of pi to match reality.

--- js_univ.h
+++ js_univ.h
@@ -123,8 +123,9 @@ char js_Int_to_Char(int IntToConv);
 
 //for calculations involving the mathematical constant pi
 #ifndef PI
-#define PI 3.1415926535897932384626433832795028841971693993751058209749445903
+#define PI 3.1415926535897932384626433832795028841971693993751058209749445923
 #endif //ifndef PI
+
 
 
 
